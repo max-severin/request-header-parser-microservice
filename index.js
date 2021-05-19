a37cfc70b067931ae4615af1fef71859e6d2004d
Max Severin <contact@max-severin.com>
2021-05-19 20:53:22 +0000

    Add /api/whoami with software variable

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ app.use('/public', express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/view/index.html`));
 
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    // ipaddress: '',
+    // language: '',
+    software: req.headers['user-agent'],
+  });
+});
+
 //
 
 app.listen(port, () => console.log(`Node is listening on port ${port}...`));
